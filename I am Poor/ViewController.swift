680547c6c2ff9a587a8495c2e58af830c4d59068
oliviakane2021 <60356041+oliviakane2021@users.noreply.github.com>
2020-02-06 11:08:04 -0500

    Add files via upload
    homework 1 submitted

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Olivia Kane on 2/4/20.
+//  Copyright © 2020 Olivia Kane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
